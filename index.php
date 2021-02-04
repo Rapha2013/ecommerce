9943f145d4a75761d8469ca4c97ae9073847afcf
Raphael Marangoni <rapha_2gtba@hotmail.com>
2021-02-04 20:17:03 -0300

    Iniciando Projeto

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Hcode\DB\Sql;
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Sql();
+
+	$result = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($result);
+	
+
+});
+
+$app->run();
+
+ ?>

78780c1d5a6904bd5ba642bcf40d24d14784defd
Raphael Marangoni <rapha_2gtba@hotmail.com>
2021-02-06 12:31:43 -0300

    Criando PageAdmin e Template Admin

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
-<?php
-
+<?php 
+ 
 require_once("vendor/autoload.php");
-
-use \Slim\Slim;
+ 
+use  \Slim\Slim;
 use \Hcode\Page;
-
+use \Hcode\PageAdmin;
+ 
 $app = new Slim();
-
+ 
 $app->config('debug', true);
-
+ 
 $app->get('/', function() {
     
 	$page = new Page();
-
-	$page->setTpl("index");
-
-
+ 
+	$page ->setTpl("index");
+ 
 });
-
+ 
+ 
+$app->get('/teste', function() {
+    
+	$page = new PageAdmin();
+ 
+	$page ->setTpl("index");
+ 
+});
+ 
 $app->run();
-
- ?>
+ 
+?>
